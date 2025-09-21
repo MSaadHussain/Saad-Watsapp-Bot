1d3671448034c381fd218631e87cd4e08dad81cb
Muhammad Saad <msh.2k21@gmail.com>
2025-09-21 11:57:18 +0500

    first commit

---
+++ plugins/main-Guru.js
@@ -0,0 +1,43 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn }) => {
+  const logo = 'https://cdn.jsdelivr.net/gh/Guru322/api@Guru/K.jpg' 
+  
+  const text = `
+╭━━━━━━━━━━━━╮
+┃ * SOCIALS *
+╰━━━━━━━━━━━━╯
+
+Join our official groups & channels to stay connected!
+
+Thank you for your support!
+`.trim()
+
+  const buttons = []
+  
+  const urls = [
+    ['GitHub Profile', 'https://github.com/Guru322'],
+    ['YouTube Channel', 'https://www.youtube.com/@Asliguru'],
+    ['Telegram Channel', 'https://t.me/NAKLI_GURU']
+  ]
+  
+  await conn.sendButton(
+    m.chat, 
+    text,
+    '© Saad\'s Bot  2025', 
+    logo, 
+    buttons, 
+    null, 
+    urls,
+    m 
+  )
+  
+  m.react('✅')
+}
+
+handler.help = ['gpguru']
+handler.tags = ['main']
+handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru']
+handler.desc = 'Get the official groups and channels of Saad\'s Bot'
+
+export default handler
---
+++ plugins/main-alive.js
@@ -0,0 +1,51 @@
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+  // Sound
+  let name = m.pushName || conn.getName(m.sender)
+  var vn = 'N/A'
+  let url = 'N/A'
+  let murl = 'N/A'
+  let img = 'N/A'
+  let con = {
+    key: {
+      fromMe: false,
+      participant: `${m.sender.split`@`[0]}@s.whatsapp.net`,
+      ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}),
+    },
+    message: {
+      contactMessage: {
+        displayName: `${name}`,
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+      },
+    },
+  }
+  let doc = {
+    audio: {
+      url: vn,
+    },
+    mimetype: 'audio/mpeg',
+    ptt: true,
+    waveform: [100, 0, 100, 0, 100, 0, 100],
+    fileName: 'Guru',
+
+    contextInfo: {
+      mentionedJid: [m.sender],
+      externalAdReply: {
+        title: 'I AM ALIVE',
+        body: 'Saad\'s Bot',
+        thumbnailUrl: img,
+        sourceUrl: 'N/A',
+        mediaType: 1,
+        renderLargerThumbnail: true,
+      },
+    },
+  }
+
+  await conn.sendMessage(m.chat, doc, { quoted: con })
+}
+
+handler.help = ['alive']
+handler.tags = ['main']
+handler.command = /^(alive)$/i
+handler.desc = 'Check if the bot is alive'
+
+export default handler
